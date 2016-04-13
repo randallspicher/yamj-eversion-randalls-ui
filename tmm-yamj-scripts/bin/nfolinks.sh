3107f88f9b1c61a3f8646497b25c81d21e1ca8d6
Randall <randallspicher@gmail.com>
2016-04-13 02:30:18 -0400

    fix

--- tmm-yamj-scripts/bin/nfolinks.sh
+++ tmm-yamj-scripts/bin/nfolinks.sh
@@ -17,16 +17,16 @@ DIRNAME=$(basename "$(pwd)")
 #echo DIRNAME = ${DIRNAME}
 
 
-if [[ -e movie.nfo ]] ; then
-	if [[ -e VIDEO_TS ]] ; then
+if [ -e movie.nfo ]; then
+	if [ -e VIDEO_TS ]; then
 		if [ ! -L "${DIRNAME}".nfo ]
 		then
 			rm "${DIRNAME}".nfo
 			ln -s movie.nfo "${DIRNAME}".nfo
 		fi	
 	fi
-	if [[ -e BDMV ]] ; then
-		if [[ ! -L "${DIRNAME}".nfo ]] ; then
+	if [ -e BDMV ]; then
+		if [ ! -L "${DIRNAME}".nfo ]; then
 			rm "${DIRNAME}".nfo
 			ln -s movie.nfo "${DIRNAME}".nfo
 		fi
@@ -36,14 +36,14 @@ fi
 
 
 
-if [[ -e tvshow.nfo ]] ; then
+if [ -e tvshow.nfo ]; then
 	name=$(grep -Po '(?i)<title>\K.*(?=</title>)' tvshow.nfo)
 
 	name=${name/&amp;/&}
 
 	echo ${name}
 		
-	if [[ ! -L "${name}.nfo" ]] ; then
+	if [ ! -L "${name}.nfo" ]; then
 		rm "${name}.nfo"
 		ln -s tvshow.nfo  "${name}.nfo"
 	fi
@@ -51,22 +51,22 @@ if [[ -e tvshow.nfo ]] ; then
 	
 	#	echo doing symlinks
 	
-	if [[ ! -L "Set_${name}_1.jpg" ]] ; then
+	if [ ! -L "Set_${name}_1.jpg" ]; then
 		rm "Set_${name}_1.jpg"
 		ln -s  poster.jpg "Set_${name}_1.jpg"
 	fi
 	
-	if [[ ! -L "Set_${name}_1.banner.jpg" ]] ; then
+	if [ ! -L "Set_${name}_1.banner.jpg" ]; then
 		rm "Set_${name}_1.banner.jpg"
 		ln -s banner.jpg "Set_${name}_1.banner.jpg" 
 	fi
 	
-	if [[ ! -L "Set_${name}_1.fanart.jpg" ]] ; then
+	if [ ! -L "Set_${name}_1.fanart.jpg" ]; then
 		rm "Set_${name}_1.fanart.jpg"
 		ln -s fanart.jpg "Set_${name}_1.fanart.jpg" 
 	fi
 	
-	if [[ ! -L "folder.jpg" ]] ; then
+	if [ ! -L "folder.jpg" ]; then
 		ln -s poster.jpg folder.jpg
 	fi
 	
@@ -75,40 +75,40 @@ if [[ -e tvshow.nfo ]] ; then
 	
 	ls -1 | grep -Pio '(season\d\d)' | while read file
 	do
-		if [[ ! -e "${file}" ]] ; then 
+		if [ ! -e "${file}" ]; then 
 			mkdir ${file}
 		fi
-		if [[ -f "${file}-poster.jpg" ]] ; then
-			if [[ ! -L "${file}/${file}.jpg" ]] ; then
+		if [ -f "${file}-poster.jpg" ]; then
+			if [ ! -L "${file}/${file}.jpg" ]; then
 				echo "linking poster ${file}-poster.jpg"
 				rm "${file}/${file}.jpg"
 				ln -s "../${file}-poster.jpg" "${file}/${file}.jpg"
 			fi
-			if [[ ! -L "${file}/folder.jpg" ]] ; then
+			if [ ! -L "${file}/folder.jpg" ]; then
 				ln -s "../${file}-poster.jpg" "${file}/folder.jpg"
 			fi
 	
 		fi
 	
-		if [[ -f "${file}/banner.jpg" -a ! -h "${file}/banner.jpg" -a ! -f "${file}-banner.jpg" ]] ; then
+		if [ -f "${file}/banner.jpg" -a ! -h "${file}/banner.jpg" -a ! -f "${file}-banner.jpg" ]; then
 			mv -v "${file}/banner.jpg" "${file}-banner.jpg"
 		fi
 	
 	
-		if [[ -f "${file}-banner.jpg" ]] ; then
+		if [ -f "${file}-banner.jpg" ]; then
 			echo "linking banner ${file}-banner.jpg"
-			if [[ ! -L "${file}/${file}.banner.jpg" ]] ; then
+			if [ ! -L "${file}/${file}.banner.jpg" ]; then
 				rm "${file}/${file}.banner.jpg"
 				ln -s "../${file}-banner.jpg" "${file}/${file}.banner.jpg"
 			fi
 		fi
 	
-		if [[ -f "${file}/fanart.jpg" -a ! -h "${file}/fanart.jpg" -a ! -f "${file}-fanart.jpg" ]] ; then
+		if [ -f "${file}/fanart.jpg" -a ! -h "${file}/fanart.jpg" -a ! -f "${file}-fanart.jpg" ]; then
 			mv -v "${file}/fanart.jpg" "${file}-fanart.jpg"
 		fi
 
-		if [[ -f "${file}-fanart.jpg" ]] ; then
-			if [[ ! -L "${file}/${file}.fanart.jpg" ]] ; then
+		if [ -f "${file}-fanart.jpg" ]; then
+			if [ ! -L "${file}/${file}.fanart.jpg" ]; then
 				echo "linking fanart ${file}-fanart.jpg"
 				rm "${file}/${file}.fanart.jpg"
 				ln -s "../${file}-fanart.jpg" "${file}/${file}.fanart.jpg"
@@ -119,27 +119,27 @@ if [[ -e tvshow.nfo ]] ; then
 
 	file="season00"
 
-	if [[ -f "season-specials-poster.jpg" ]] ; then
-		if [[ ! -L "${file}/${file}.jpg" ]] ; then
+	if [ -f "season-specials-poster.jpg" ]; then
+		if [ ! -L "${file}/${file}.jpg" ]; then
 			echo "linking poster season-specials-poster.jpg"
 			rm "${file}/${file}.jpg"
 			ln -s "../season-specials-poster.jpg" "${file}/${file}.jpg"
 		fi
-		if [[ ! -L "${file}/folder.jpg" ]] ; then
+		if [ ! -L "${file}/folder.jpg" ]; then
 			ln -s "../season-specials-poster.jpg" "${file}/folder.jpg"
 		fi
 	fi
 
-	if [[ -f "season-specials-banner.jpg" ]] ; then
-		if [[ ! -L "${file}/${file}.banner.jpg" ]] ; then
+	if [ -f "season-specials-banner.jpg" ]; then
+		if [ ! -L "${file}/${file}.banner.jpg" ]; then
 			echo "linking banner season-specials-banner.jpg"
 			rm "${file}/${file}.banner.jpg"
 			ln -s "../season-specials-banner.jpg" "${file}/${file}.banner.jpg"
 		fi
 	fi
 
-	if [[ -f "season-specials-fanart.jpg" ]] ; then
-		if [[ ! -L "${file}/${file}.fanart.jpg" ]] ; then
+	if [ -f "season-specials-fanart.jpg" ]; then
+		if [ ! -L "${file}/${file}.fanart.jpg" ]; then
 			echo "linking fanart ${file}-fanart.jpg"
 			rm "${file}/${file}.fanart.jpg"
 			ln -s "../season-specials-fanart.jpg" "${file}/${file}.fanart.jpg"
@@ -155,7 +155,7 @@ fi
 #fi
 
 
-if [[ -e movie.nfo ]] ; then
+if [ -e movie.nfo ]; then
 	name=$(grep -Po '(?i)<set>\K.*(?=</set>)' movie.nfo)
 
 
@@ -164,30 +164,30 @@ if [[ -e movie.nfo ]] ; then
 	echo SET=${name}
 	
 
-	if [[ -e movieset-poster.jpg ]] ; then
-		if [[ ! -L "Set_${name}_1.jpg" ]] ; then
+	if [ -e movieset-poster.jpg ]; then
+		if [ ! -L "Set_${name}_1.jpg" ]; then
 			rm "Set_${name}_1.jpg"
 			ln -s  movieset-poster.jpg "Set_${name}_1.jpg"
 		fi
 	fi
 
-	if [[ -e movieset-folder.jpg ]] ; then
-		if [[ ! -L "Set_${name}_1.jpg" ]] ; then
+	if [ -e movieset-folder.jpg ]; then
+		if [ ! -L "Set_${name}_1.jpg" ]; then
 			rm "Set_${name}_1.jpg"
 			ln -s  movieset-folder.jpg "Set_${name}_1.jpg"
 		fi
 	fi
 
 
-	if [[ -e movieset-banner.jpg ]] ; then
-		if [[ ! -L "Set_${name}_1.banner.jpg" ]] ; then
+	if [ -e movieset-banner.jpg ]; then
+		if [ ! -L "Set_${name}_1.banner.jpg" ]; then
 			rm "Set_${name}_1.banner.jpg"
 			ln -s movieset-banner.jpg "Set_${name}_1.banner.jpg" 
 		fi
 	fi
 	
-	if [[ -e movieset-fanart.jpg ]] ; then
-		if [[ ! -L "Set_${name}_1.fanart.jpg" ]] ; then
+	if [ -e movieset-fanart.jpg ]; then
+		if [ ! -L "Set_${name}_1.fanart.jpg" ]; then
 			rm "Set_${name}_1.fanart.jpg"
 			ln -s movieset-fanart.jpg "Set_${name}_1.fanart.jpg" 
 		fi
@@ -200,7 +200,7 @@ fi
 		#echo detected ${file}
 		vidfile=${file/-thumb.jpg/.videoimage.jpg}
 		#echo new name: ${vidfile}
-		if [[ ! -L "${vidfile}" ]] ; then
+		if [ ! -L "${vidfile}" ]; then
 			rm "${vidfile}"
 			ln -s "${file}" "${vidfile}"
 		fi
