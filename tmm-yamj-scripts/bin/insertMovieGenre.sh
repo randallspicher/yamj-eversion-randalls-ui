d1e9266a81b1d82eee235d8c328efc76573306b3
Randall <randallspicher@gmail.com>
2019-03-05 01:17:18 -0500

    just add Genres, instead of replacing

--- tmm-yamj-scripts/bin/insertMovieGenre.sh
+++ tmm-yamj-scripts/bin/insertMovieGenre.sh
@@ -14,22 +14,23 @@ for DIR in * ; do
 		echo parsing "${DIR}"
 		for FILE in $( find "${DIR}" -name 'movie.nfo') ; do
   			if [[ $(grep -PiL "<genre>${DIR}</genre>" "${FILE}") ]] ; then
-				echo Wrong GENRE in ${FILE} 
+				#echo Wrong GENRE in ${FILE} 
 				#remove any genre tag that may already exist
-				sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
+				#sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
 				# insert correct genre tag
+				echo adding genre ${DIR} to ${FILE}
 				sed -i "s,<movie>,<movie><genre>${DIR}</genre>," ${FILE}
 			#else 
 			#	echo Correct GENRE ${FILE}
 			fi
 
-			if [[ $(grep -Pic "<genre>" "${FILE}") -gt 1 ]] ; then
-				echo found too many genres in ${FILE}
-				#remove any genre tag that may already exist
-				sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
-				# insert correct genre tag
-				sed -i "s,<movie>,<movie><genre>${DIR}</genre>," ${FILE}
-			fi 
+			#if [[ $(grep -Pic "<genre>" "${FILE}") -gt 1 ]] ; then
+			#	echo found too many genres in ${FILE}
+			#	#remove any genre tag that may already exist
+			#	sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
+			#	# insert correct genre tag
+			#	sed -i "s,<movie>,<movie><genre>${DIR}</genre>," ${FILE}
+			#fi 
 
 		done 
 	fi
--- tmm-yamj-scripts/bin/insertTVGenre.sh
+++ tmm-yamj-scripts/bin/insertTVGenre.sh
@@ -12,25 +12,27 @@ IFS=$(echo -en "\n\b")
 for DIR in * ; do
 	if [[ -d "${DIR}" ]] ;  then
 		echo parsing "${DIR}"
-		GENRE="TV ${DIR}"
+		#GENRE="TV ${DIR}"
+		GENRE="${DIR}"
 		for FILE in $( find "${DIR}" -name 'tvshow.nfo') ; do
   			if [[ $(grep -PiL "<genre>${GENRE}</genre>" "${FILE}") ]] ; then
-				echo Wrong GENRE in ${FILE} 
+				#echo Wrong GENRE in ${FILE} 
 				#remove any genre tag that may already exist
-				sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
+				#sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
 				# insert correct genre tag
+				echo adding genre ${GENRE} to ${FILE}
 				sed -i "s,<tvshow>,<tvshow><genre>${GENRE}</genre>," ${FILE}
 			#else 
 				#echo Correct GENRE ${FILE}
 			fi
 			
-			if [[ $(grep -Pic "<genre>" "${FILE}") -gt 1 ]] ; then
-				echo found too many genres in ${FILE}
-				#remove any genre tag that may already exist
-				sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
-				# insert correct genre tag
-				sed -i "s,<tvshow>,<tvshow><genre>${GENRE}</genre>," ${FILE}
-			fi 
+			#if [[ $(grep -Pic "<genre>" "${FILE}") -gt 1 ]] ; then
+			#	echo found too many genres in ${FILE}
+			#	#remove any genre tag that may already exist
+			#	sed -i "s,<genre>[^<]*</genre>,,"  ${FILE}
+			#	# insert correct genre tag
+			#	sed -i "s,<tvshow>,<tvshow><genre>${GENRE}</genre>," ${FILE}
+			#fi 
 		done 
 	fi
 done
